1a322c2020b0636418c47c267b79f55a9817005d
latte488 <latte488icloud.com>
2020-06-11 18:25:34 +0900

    fix download.sh

--- download.sh
+++ download.sh
@@ -4,6 +4,10 @@ DATASET_URL='http://slazebni.cs.illinois.edu/research/uiuc_texture_dataset.zip'
 DATASET_ZIP=`basename $DATASET_URL`
 SAVE_PATH='data/uiuc_texture_dataset'
 
+if [ -d $SAVE_PATH ]; then
+    rm -rf $SAVE_PATH
+fi
+
 wget $DATASET_URL
 mkdir -p $SAVE_PATH
 unzip -d $SAVE_PATH $DATASET_ZIP
